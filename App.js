099a1f0c83f044fe29117bafba4cd1a2d28557d9
glznn <gsm004@ucsd.edu>
2026-05-06 10:54:47 -0700

    Add bottom navigation bar

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, Image, View } from 'react-native';
+import BottomNav from './BottomNav';
+
+const {width, height} = Dimensions.get('window');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.top}>
+        <Text style={styles.topText}>Hey, Raph</Text>
+        <View style={styles.streak}>
+          <Text>
+            6
+          </Text>
+          <Image
+            source={require('./assets/flame.png')}
+            style={styles.flame}
+          />
+        </View>
+      </View>
+      <Text>Open up App.js to start working on your</Text>
       <StatusBar style="auto" />
+      <BottomNav/>
+      <Text>Testing</Text>
     </View>
   );
 }
@@ -13,8 +30,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fbfaf5',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  top: {
+    position: 'absolute',
+    top: height * 0.1,
+    height: height * 0.14,
+    backgroundColor: '#A4CDF7',
+    width: width * 0.98,
+    borderRadius: 12,
+  },
+  topText: {
+    width: width * 0.2,
+    height: height * 0.1,
+    marginTop: 16,
+    marginLeft: 16,
+    fontFamily: 'Helvetica Neue',
+    fontWeight: 'bold',
+    fontSize: '35',
+    color: 'white',
+  },
+  flame: {
+    opacity: 0.8,
+    width: 31,
+    height: 38,
+  }
 });
---
+++ BottomNav.jsx
@@ -0,0 +1,75 @@
+import {
+  Dimensions,
+  FlatList,
+  StyleSheet,
+  Text,
+  Image,
+  View,
+  TouchableOpacity,
+} from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
+function BottomNav() {
+  const menu = [
+    { id: "1", name: "Home" },
+    { id: "2", name: "Calendar" },
+    { id: "3", name: "center-btn" },
+    { id: "4", name: "Progress" },
+    { id: "5", name: "Search" },
+  ];
+  return (
+    <View style={styles.container}>
+      {menu.map((item) =>
+        item.name === "center-btn" ? (
+          <TouchableOpacity key={item.id} style={styles.item}>
+            <View style={styles.btn}>
+              <Image
+                source={require("./assets/dumbbell.png")}
+                style={styles.dumbbell}
+              />
+            </View>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity key={item.id} style={styles.item}>
+            <Text>{item.name}</Text>
+          </TouchableOpacity>
+        ),
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    overflow: "visible",
+    flexDirection: "row",
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    borderTopWidth: 1,
+    borderTopColor: "#e0e0e0",
+    paddingVertical: 25,
+  },
+  item: {
+    overflow: "visible",
+    width: width / 5,
+    alignItems: "center",
+  },
+  btn: {
+    height: 60,
+    width: 60,
+    backgroundColor: "#775FFF",
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: -20,
+  },
+  dumbbell: {
+    width: 35,
+    height: 35,
+  },
+});
+
+export default BottomNav;
